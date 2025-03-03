93e1d760b3f52ddc27e460d368bed5e7267a3f10
whentheimpo <cerdokk7@gmail.com>
2025-03-02 16:16:07 -0800

    Updated bot code to include intents

--- bot.py
+++ bot.py
@@ -9,11 +9,17 @@ load_dotenv()
 # Get the token from the environment
 token = os.getenv("DISCORD_TOKEN")
 
-# Set up the bot
-bot = commands.Bot(command_prefix="!")
+# Create intents to specify which events your bot will listen to
+intents = discord.Intents.default()
+intents.message_content = True  # Enables access to message content (if your bot needs it)
+
+# Pass intents to the bot
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}')
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game("Your bot is running!"))
 
+# Start the bot with the token
 bot.run(token)
